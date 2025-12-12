c11b975640c19424c2029f91defbfad645683fbb
Nikola Krezeski <nkrezeski1@gmail.com>
2025-12-12 11:43:53 +0100

    feat: add day-1 part 2

--- problems/2025/day-1/solution.go
+++ problems/2025/day-1/solution.go
@@ -24,7 +24,33 @@ func getNumberAfterRotation(currentNumber int, rotation string) int {
 		newNumber = currentNumber + distance
 	}
 
-	return newNumber % 100
+	return ((newNumber % 100) + 100) % 100
+}
+
+func countNumberOfZeroJumps(currentNumber int, rotation string) int {
+	direction := rotation[:1]
+	distance, _ := strconv.Atoi(rotation[1:])
+
+	var firstHit int
+	if direction == "L" {
+		if currentNumber == 0 {
+			firstHit = 100
+		} else {
+			firstHit = currentNumber
+		}
+	} else {
+		if currentNumber == 0 {
+			firstHit = 100
+		} else {
+			firstHit = 100 - currentNumber
+		}
+	}
+
+	if distance < firstHit {
+		return 0
+	}
+
+	return (distance-firstHit)/100 + 1
 }
 
 func partOne(input interface{}) string {
@@ -42,6 +68,21 @@ func partOne(input interface{}) string {
 	return strconv.Itoa(password)
 }
 
+func partTwo(input interface{}) string {
+	parsedInput := input.(Input)
+	currentNumber := 50
+	password := 0
+
+	for _, line := range parsedInput.list {
+		numberOfZeroJumps := countNumberOfZeroJumps(currentNumber, line)
+		currentNumber = getNumberAfterRotation(currentNumber, line)
+
+		password += numberOfZeroJumps
+	}
+
+	return strconv.Itoa(password)
+}
+
 func processInput(input []string) Input {
 	return Input{
 		list: input,
@@ -51,5 +92,5 @@ func processInput(input []string) Input {
 func main() {
 	input := utils.ReadInput()
 
-	utils.Run(processInput(input), []utils.Problem{{Problem: partOne}})
+	utils.Run(processInput(input), []utils.Problem{{Problem: partOne}, {Problem: partTwo}})
 }
